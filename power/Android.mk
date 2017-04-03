ec5a7c71d6598d2fbd97af9b98cc31ce54ae66e9
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-04-03 18:38:37 -0500

    add support of mpctlv3 on msm8953
    Change-Id: Id558a7bdc5dc8cdc96725f67cb383e151fe38979
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- power/Android.mk
+++ power/Android.mk
@@ -51,6 +51,7 @@ endif
 
 ifeq ($(call is-board-platform-in-list,msm8953), true)
 LOCAL_SRC_FILES += power-8953.c
+LOCAL_CFLAGS += -DMPCTLV3
 endif
 
 ifeq ($(call is-board-platform-in-list,msm8937), true)
